c8ae68f823635526c1b714915524314d301168bf
anthony cantor <ar.cantor@gmail.com>
2015-03-13 20:16:52 -0700

    added goto-prev-mark function

--- hiraishin.el
+++ hiraishin.el
@@ -28,21 +28,40 @@
   (> (marker-position mk) (point))
 )
 
+(defun hiraishin-mark-lt-point (mk)
+  (< (marker-position mk) (point))
+)
+
 (defun hiraishin-mark-next ()
-  (let* ((p (point))
-         (marks (hiraishin-mark-list))
+  (let* ((marks (hiraishin-mark-list))
          (nextmark (hiraishin-find-mark marks 'hiraishin-mark-gt-point)))
-    (if nextmark nextmark (car marks))    
+    (or nextmark (car marks))
+  )
+)
+
+(defun hiraishin-mark-prev ()
+  (let* ((marks (reverse (hiraishin-mark-list)))
+         (prevmark (hiraishin-find-mark marks 'hiraishin-mark-lt-point)))
+    (or prevmark (car marks))
+  )
+)
+
+(defun hiraishin-goto (mark-fn)
+  (let ((mk (funcall mark-fn)))
+    (when mk
+          (goto-char (marker-position mk))
+    )
   )
 )
 
 (defun hiraishin-goto-next-mark ()
   (interactive)
-  (let ((nextmark (hiraishin-mark-next)))
-    (when nextmark
-          (goto-char (marker-position nextmark))
-    )
-  )
+  (hiraishin-goto 'hiraishin-mark-next)
+)
+
+(defun hiraishin-goto-prev-mark ()
+  (interactive)
+  (hiraishin-goto 'hiraishin-mark-prev)
 )
 
 ;;;###autoload
@@ -52,11 +71,9 @@
   :keymap (let ((map (make-sparse-keymap)))
             (define-key map (kbd "C-c m") 'hiraishin-add-mark)
             (define-key map (kbd "C-c n") 'hiraishin-goto-next-mark)
+            (define-key map (kbd "C-c p") 'hiraishin-goto-prev-mark)
             map)
   :global t
 )
-
-;;;###autoload
-(add-hook 'text-mode-hook 'hiraishin-mode)
 
 (provide 'hiraishin-mode)
